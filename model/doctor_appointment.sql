c5930236eccbdd11d750beadf3fff780e2cab785
Jpgomera19 <jpgomera19@gmail.com>
2023-11-08 23:37:38 +0800

    DATABASE

--- model/doctor_appointment.sql
+++ model/doctor_appointment.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2023 at 10:05 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 5.6.40
+-- Generation Time: Nov 08, 2023 at 04:36 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `doctor_appointment`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `appointment_date_start` date NOT NULL,
+  `appointment_date_end` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `user_id`, `appointment_date_start`, `appointment_date_end`, `date_created`) VALUES
+(1, 1, '2023-11-16', '0000-00-00 00:00:00', '2023-11-08 21:21:48'),
+(2, 1, '2023-11-17', '2023-11-08 23:06:40', '2023-11-08 23:06:40');
 
 -- --------------------------------------------------------
 
@@ -39,22 +60,28 @@ CREATE TABLE `user` (
   `division` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `user_type` varchar(50) NOT NULL DEFAULT 'USER',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `pcn_id_number`, `firstname`, `middlename`, `lastname`, `email`, `contact_number`, `division`, `username`, `password`, `date_created`) VALUES
-(1, 0, '', '', '', '', 0, '', 'jpgomera123', '123456789', '2023-11-08 16:04:18'),
-(2, 123, 'James Philip', 'Amante', 'Gomera', 'jpgomera19@gmail.com', 2147483647, 'STRAT', 'jpgomera19', 'Jp123456789', '2023-11-08 16:48:34'),
-(3, 123, 'asdsad', 'asdas', 'asdad', 'jpgomera19@gmail.com', 123123, 'BD3', 'asdasd', 'asdasdasF312213', '2023-11-08 16:49:47'),
-(4, 123123, 'James Philip', 'asdada', 'adasd', '123123@gmail.com', 1312312, 'BD2', '31312', '123123123Dwdfqwd', '2023-11-08 16:50:40');
+INSERT INTO `user` (`id`, `pcn_id_number`, `firstname`, `middlename`, `lastname`, `email`, `contact_number`, `division`, `username`, `password`, `user_type`, `date_created`) VALUES
+(1, 12345678, 'James Philip', 'Amante', 'Gomera', 'jpgomera19@gmail.com', 2147483647, 'STRAT', 'jpgomera19', '$2y$10$ohpIf7Jsswls1R.D50HoVuBhl.upjHYdXKJXnrU4GmGgc4vmHcJi6', 'USER', '2023-11-08 20:17:47'),
+(3, 123456, 'jerryboy', 'amante', 'gomera', 'jerryboy123@gmail.com', 2147483647, 'HR', 'jerryboy123', '$2y$10$twitJnJTU3kEaF2g0jIc8eTBrB/o6CMaNnfogrojC1eZlkZNg3rNK', 'USER', '2023-11-08 20:32:33');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `user`
@@ -67,10 +94,26 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
